0062e4fa09b2d90e4a19742117d55508bb9207ed
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-04 10:00:17 -0500

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+  array = []
+  (3...num).each do |i|
+    if i % 3 == 0 || i % 5 == 0
+      array << i
+    end
+  end
+  array
+end
+
+# def collect_multiples(num)
+#   (3...num).select do |i|
+#     if i % 3 == 0 || i % 5 == 0
+#     end
+#   end
+# end
+
+def sum_multiples(num)
+  collect_multiples(num).inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  # def collect_multiples
+  #   (3...@num).select do |i|
+  #     if i % 3 == 0 || i % 5 == 0
+  #     end
+  #   end
+  # end
+
+  def collect_multiples
+    array = []
+    (3...@num).each do |i|
+      if i % 3 == 0 || i % 5 == 0
+        array << i
+      end
+    end
+    array
+  end
+
+  def sum_multiples
+    collect_multiples.inject(0, :+)
+  end
+
+
+end
